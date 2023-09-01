fc00c12c0a68ec27a077c0c437e6cdd7ee73b880
alexdemichieli <alexdemichieli@github.com>
2023-08-31 20:24:29 -0700

    adding changes to dockerfile

--- src/App.js
+++ src/App.js
@@ -24,16 +24,8 @@ const App = () => {
 
   return (
     <div>
-      <h1>Welcome to 1we1efgerfg</h1>
+      <h1>Welcome</h1>
       <iframe src={`http://localhost/etherpad/p/${padId}#L4?showChat=false&showLineNumbers=false`} width='600' height='400' />
-
-      <iframe
-        title="Etherpad"
-        src="http://localhost/etherpad/" // Replace this with the correct URL of your Etherpad server
-        width="100%"
-        height="600px"
-        frameBorder="0"
-      />
     </div>
   );
 }
